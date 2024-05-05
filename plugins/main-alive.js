0d743e46ab1112bd11f052ea6f3811507695edbf
UchihaLord <165956346+UchihaLord@users.noreply.github.com>
2024-05-05 05:01:40 +0200

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,9 +5,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./Assets/ALIVE.mp3"
-    let url = "https://github.com/abrahamdw882"
+    let url = "https://github.com/uchihalord"
     let murl = "https://YouTube.com"
-    let img = "https://telegra.ph/file/b1b157e944010efebf1d7.jpg"
+    let img = "https://telegra.ph/file/f3e71b73696ab9e1841dd.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -21,8 +21,8 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "THE XLICON-V2 IS ALIVE 🌟",
-          body: "XLICON BOT",
+          title: "THE COPYNINJA-X IS ALIVE 🌟",
+          body: "COPYNINJA BOT",
           thumbnailUrl: img,
           sourceUrl: 'https://chat.whatsapp.com/C4ivwZKuh5bLJkqfYNPQsk',
           mediaType: 1,
